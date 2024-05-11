329db2b714e5726acc5ded1fa2faefb91978cb71
RaffA <raffa@students.ubc.ca>
2024-05-10 20:58:08 -0700

    implemented the functionality for the web app

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
+from flask import Flask, render_template, request
 import requests
 
-api_key = 'e47a6b393fa3f32e5fa313044a544c1d'
+app = Flask(__name__)
 
-user_input = input("Enter city: ")
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        api_key = 'e47a6b393fa3f32e5fa313044a544c1d'
+        new_city = request.form.get('newCity')
+        weather_data = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={new_city}&units=metric&APPID={api_key}"
+        )
 
-weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&APPID={api_key}")
+        if weather_data.json()['cod'] == '404':
+            weather = "City not found"
+            temp = None
+        else:
+            weather = weather_data.json()['weather'][0]['main']
+            temp = round(weather_data.json()['main']['temp'])
 
-if weather_data.json()['cod'] == '404':
-    print("City not found")
-else:
-    weather = weather_data.json()['weather'][0]['main']
-    temp = round(weather_data.json()['main']['temp'])
+        return render_template("weather.html", city=new_city, weather=weather, temp=temp)
+    else:
+        return render_template("home.html")
 
-    print(f"Current weather in {user_input}:")
-    print(f"{weather}")
-    print(f"{temp} °C")
+if __name__ == "__main__":
+    app.run(debug=True)
